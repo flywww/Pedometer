7f483de27b740ffcef0d50ef7b8909274b20c08e
stanley <flywww004@gmail.com>
2014-06-27 21:53:21 +0800

    Background mode done

---
+++ Pedometer/HelpTools.h
@@ -0,0 +1,13 @@
+//
+//  HelpTools.h
+//  Pedometer
+//
+//  Created by 林盈志 on 6/27/14.
+//  Copyright (c) 2014 林盈志. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelpTools : NSObject
+
+@end
---
+++ Pedometer/HelpTools.m
@@ -0,0 +1,13 @@
+//
+//  HelpTools.m
+//  Pedometer
+//
+//  Created by 林盈志 on 6/27/14.
+//  Copyright (c) 2014 林盈志. All rights reserved.
+//
+
+#import "HelpTools.h"
+
+@implementation HelpTools
+
+@end
